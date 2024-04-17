afd4a998230453fb792ea0fdefc6b918a11cec9b
_im'kavindu <65580465+KavinduUoM20@users.noreply.github.com>
2024-04-17 11:35:59 +0530

    Data Ingestion Added

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from mlopsproject import logger
-# from mlopsproject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from mlopsproject.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
 # from mlopsproject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 # from mlopsproject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 # from mlopsproject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 # from mlopsproject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
+STAGE_NAME = "Data Ingestion Stage"
 
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
