8b20afce4b6755dd033ddf066dcc732c319549ea
_im'kavindu <65580465+KavinduUoM20@users.noreply.github.com>
2024-04-19 06:15:01 +0530

    Model Trainer Added

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from mlopsproject import logger
 from mlopsproject.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
 from mlopsproject.pipeline.stage02_data_validation import DataValidationTrainingPipeline
-# from mlopsproject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from mlopsproject.pipeline.stage03_data_transformtion import DataTransformationTrainingPipeline
+from mlopsproject.pipeline.stage04_model_trainer import ModelTrainerTrainingPipeline
 # from mlopsproject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 # from mlopsproject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
@@ -26,3 +27,27 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+STAGE_NAME = "Data Transformation Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataTransformationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Model Trainer Stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = ModelTrainerTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
